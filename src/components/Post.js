41172a73bb4f9eb666a16d21a6cfa04441b134ca
James Cho <hwisoocho@gmail.com>
2019-03-17 09:53:46 -0700

    setup project, create components

---
+++ src/components/Post.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import React, { Component } from "react";
+
+export default class Post extends Component {
+  render() {
+    return <div />;
+  }
+}
---
+++ src/components/Postlist.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import React, { Component } from "react";
+
+export default class Postlist extends Component {
+  render() {
+    return <div />;
+  }
+}
